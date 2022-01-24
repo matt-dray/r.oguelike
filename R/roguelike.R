8db00482bed6c0bfabd3d7ac99934395f8758637
Matt Dray <mwdray@gmail.com>
2022-01-24 21:44:51 +0000

    Add enemy and apple with interaction effects, bump 9001

--- R/roguelike.R
+++ R/roguelike.R
@@ -1,9 +1,13 @@
 
 #' Add Objects To A Room
-#' @param object Character. Object to place.
 #' @param room Matrix. 2D room layout.
+#' @param object Character. Object to place.
 #' @noRd
-.add_object <- function(room, object = c("@", "$", "E")) {
+.add_object <- function(room, object = c("@", "$", "E", "a")) {
+
+  if (!inherits(room, "matrix")) {
+    stop("Argument 'room' must be a matrix.")
+  }
 
   object <- match.arg(object)
 
@@ -20,6 +24,11 @@
 #' @noRd
 .make_room <- function(room_size_limit = 6:10) {
 
+  if (!is.numeric(room_size_limit)) {
+    stop("Argument 'room_size_limit' must be a numeric vector.")
+
+  }
+
   room_x <- sample(room_size_limit, 1)
   room_y <- sample(room_size_limit, 1)
   room_size <- room_x * room_y
@@ -33,6 +42,7 @@
 
   room_2d <- .add_object(room_2d, "$")
   room_2d <- .add_object(room_2d, "E")
+  room_2d <- .add_object(room_2d, "a")
   room_2d <- .add_object(room_2d, "@")
 
   return(room_2d)
@@ -44,24 +54,40 @@
 #' @noRd
 .cat_room <- function(room) {
 
+  if (!inherits(room, "matrix")) {
+    stop("Argument 'room' must be a matrix.")
+  }
+
   for (i in 1:nrow(room)) {
     cat(room[i, ], "\n")
   }
 
 }
 
-#' Concatenate And Print A Room Matrix
-#' @param room Matrix. 2D room layout.
-#' @param turns NUmeric. Starting amount of turns.
-#' @param gold Numeric. Starting amount of turns
+#' Concatenate And Print A Stats Bar
+#' @param turns Numeric. Count of turns taken.
+#' @param hp Numeric. Count of HP remaining
+#' @param gold Numeric. Count of gold accumulated.
 #' @noRd
-.move_player <- function(room, turns, gold) {
+.cat_stats <- function(turns, hp, gold) {
 
-  stats <- paste("Turns:", turns, "| Gold:", gold, "\n")
+  stats <- paste("HP:", hp, "| G:", gold, "\n")
   cat(stats)
+
+}
+
+#' Move Player Character And Increment Counters
+#' @param room Matrix. 2D room layout.
+#' @noRd
+.move_player <- function(room) {
+
+  if (!inherits(room, "matrix")) {
+    stop("Argument 'room' must be a matrix.")
+  }
 
   player_loc <- which(room == "@")
   room[player_loc] <- "."
+
   room_y_max <- nrow(room)
 
   keypress_support <- keypress::has_keypress_support()
@@ -75,39 +101,23 @@
     kp <- readline("Direction (up, down, left, right): ")
   }
 
-  if (kp == "up") {
+  if (kp %in% c("up", "down", "left", "right")) {
 
-    move_to <- player_loc - 1
-
-    if (room[move_to] != "#") {
-      player_loc <- move_to
+    if (kp == "up") {
+      move_to <- player_loc - 1
     }
 
-  }
-
-  if (kp == "down") {
-
-    move_to <- player_loc + 1
-
-    if (room[move_to] != "#") {
-      player_loc <- move_to
+    if (kp == "down") {
+      move_to <- player_loc + 1
     }
 
-  }
-
-  if (kp == "right") {
-
-    move_to <- player_loc + room_y_max
-
-    if (room[move_to] != "#") {
-      player_loc <- move_to
+    if (kp == "right") {
+      move_to <- player_loc + room_y_max
     }
 
-  }
-
-  if (kp == "left") {
-
-    move_to <- player_loc - room_y_max
+    if (kp == "left") {
+      move_to <- player_loc - room_y_max
+    }
 
     if (room[move_to] != "#") {
       player_loc <- move_to
@@ -128,12 +138,22 @@
 #' @param max_turns Numeric. How many turns? Default \code{Inf}.
 #'
 #' @details If your terminal supports the 'keypress' package, then you can use
-#'   single keypresses as input, otherwise you will have to type words at the
-#'   prompt. Use \code{\link[keypress]{has_keypress_support}} to see if
-#'   'keypress' is supported.
+#'   a single keypresses as input (e.g. the up arrow key), otherwise you will
+#'   have to type words at the prompt. Use
+#'   \code{\link[keypress]{has_keypress_support}} to see if 'keypress' is
+#'   supported.
 #'
-#' @return Nothing. Clears the console and prints to the console with
-#'   \code{cat}.
+#'   Symbols are as follows:
+#'   \itemize{
+#'     \item{\code{.}} {tile}
+#'     \item{\code{#}} {wall}
+#'     \item{\code{@}} {player}
+#'     \item{\code{$}} {gold (+1 to +3 G)}
+#'     \item{\code{E}} {enemy (-1 HP)}
+#'     \item{\code{a}} {food (+1 HP)}
+#'   }
+#'
+#' @return Nothing. Clears the console and prints to it with \code{cat}.
 #' @export
 #'
 #' @examples \dontrun{start_game()}
@@ -154,24 +174,50 @@ start_game <- function(max_turns = Inf) {
     cat("\014")
   }
 
+  turns <- 0
+  gold  <- 0
+  hp    <- 10
+
   room <- .make_room()
   .cat_room(room)
-
-  gold <- 0
-  turns <- 0
+  .cat_stats(turns, hp, gold)
 
   while (turns < max_turns) {
 
-    gold_loc <- which(room == "$")
+    gold_loc  <- which(room == "$")
+    enemy_loc <- which(room == "E")
+    food_loc  <- which(room == "a")
 
-    room <- .move_player(room, turns, gold)
+    room <- .move_player(room)
 
     player_loc <- which(room == "@")
 
     if (length(gold_loc) != 0) {
 
       if (player_loc == gold_loc) {
-        gold <- gold + sample(1:10, 1)
+        gold <- gold + sample(1:3, 1)
+      }
+
+    }
+
+    if (length(enemy_loc) != 0) {
+
+      if (player_loc == enemy_loc) {
+        hp <- hp - 1
+      }
+
+    }
+
+    if (length(food_loc) != 0) {
+
+      if (player_loc == food_loc) {
+
+        hp <- hp + 1
+
+        if (hp > 10) {
+          hp <- 10
+        }
+
       }
 
     }
@@ -187,6 +233,7 @@ start_game <- function(max_turns = Inf) {
     }
 
     .cat_room(room)
+    .cat_stats(turns, hp, gold)
 
   }
 
