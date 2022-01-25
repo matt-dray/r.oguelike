42698204745b636915cee2ee7f8404e1333abaff
Matt Dray <mwdray@gmail.com>
2022-01-25 22:27:06 +0000

    Separate keypress fn, narrative msg, split out utils

--- R/roguelike.R
+++ R/roguelike.R
@@ -1,136 +1,4 @@
 
-#' Add Objects To A Room
-#' @param room Matrix. 2D room layout.
-#' @param object Character. Object to place.
-#' @noRd
-.add_object <- function(room, object = c("@", "$", "E", "a")) {
-
-  if (!inherits(room, "matrix")) {
-    stop("Argument 'room' must be a matrix.")
-  }
-
-  object <- match.arg(object)
-
-  empty_tiles <- which(room == ".")
-  sample_tile <- sample(empty_tiles, 1)
-  room[sample_tile] <- object
-
-  return(room)
-
-}
-
-#' Generate A Room From Sampled Dimensions
-#' @param what Numeric vector. Values that room height and width can take.
-#' @noRd
-.make_room <- function(room_size_limit = 6:10) {
-
-  if (!is.numeric(room_size_limit)) {
-    stop("Argument 'room_size_limit' must be a numeric vector.")
-
-  }
-
-  room_x <- sample(room_size_limit, 1)
-  room_y <- sample(room_size_limit, 1)
-  room_size <- room_x * room_y
-
-  room_1d <- rep(".", room_size)
-
-  room_2d <- matrix(room_1d, nrow = room_x, ncol = room_y)
-
-  room_2d[c(1, nrow(room_2d)), ] <- "#"
-  room_2d[, c(1, ncol(room_2d))] <- "#"
-
-  room_2d <- .add_object(room_2d, "$")
-  room_2d <- .add_object(room_2d, "E")
-  room_2d <- .add_object(room_2d, "a")
-  room_2d <- .add_object(room_2d, "@")
-
-  return(room_2d)
-
-}
-
-#' Concatenate And Print A Room Matrix
-#' @param room Matrix. 2D room layout.
-#' @noRd
-.cat_room <- function(room) {
-
-  if (!inherits(room, "matrix")) {
-    stop("Argument 'room' must be a matrix.")
-  }
-
-  for (i in 1:nrow(room)) {
-    cat(room[i, ], "\n")
-  }
-
-}
-
-#' Concatenate And Print A Stats Bar
-#' @param turns Numeric. Count of turns taken.
-#' @param hp Numeric. Count of HP remaining
-#' @param gold Numeric. Count of gold accumulated.
-#' @noRd
-.cat_stats <- function(turns, hp, gold) {
-
-  stats <- paste("HP:", hp, "| G:", gold, "\n")
-  cat(stats)
-
-}
-
-#' Move Player Character And Increment Counters
-#' @param room Matrix. 2D room layout.
-#' @noRd
-.move_player <- function(room) {
-
-  if (!inherits(room, "matrix")) {
-    stop("Argument 'room' must be a matrix.")
-  }
-
-  player_loc <- which(room == "@")
-  room[player_loc] <- "."
-
-  room_y_max <- nrow(room)
-
-  keypress_support <- keypress::has_keypress_support()
-
-  if (keypress_support) {
-    cat("Use arrow keys")
-    kp <- keypress::keypress()
-  }
-
-  if (!keypress_support) {
-    kp <- readline("Direction (up, down, left, right): ")
-  }
-
-  if (kp %in% c("up", "down", "left", "right")) {
-
-    if (kp == "up") {
-      move_to <- player_loc - 1
-    }
-
-    if (kp == "down") {
-      move_to <- player_loc + 1
-    }
-
-    if (kp == "right") {
-      move_to <- player_loc + room_y_max
-    }
-
-    if (kp == "left") {
-      move_to <- player_loc - room_y_max
-    }
-
-    if (room[move_to] != "#") {
-      player_loc <- move_to
-    }
-
-  }
-
-  room[player_loc] <- "@"
-
-  return(room)
-
-}
-
 #' Start A 'r.oguelike' Game
 #'
 #' Clears the console and starts a game of 'r.oguelike'.
@@ -181,6 +49,7 @@ start_game <- function(max_turns = Inf) {
   room <- .make_room()
   .cat_room(room)
   .cat_stats(turns, hp, gold)
+  cat("Start game")
 
   while (turns < max_turns) {
 
@@ -188,14 +57,23 @@ start_game <- function(max_turns = Inf) {
     enemy_loc <- which(room == "E")
     food_loc  <- which(room == "a")
 
-    room <- .move_player(room)
+    kp <- .accept_keypress()
+    room <- .move_player(room, kp)
+
+    msg <- paste("Moved", kp)
 
     player_loc <- which(room == "@")
 
     if (length(gold_loc) != 0) {
 
+      gold_rand <- sample(1:3, 1)
+
       if (player_loc == gold_loc) {
-        gold <- gold + sample(1:3, 1)
+
+        gold <- gold + gold_rand
+
+        msg <- paste0("Found gold (+", gold_rand, " G)")
+
       }
 
     }
@@ -203,7 +81,11 @@ start_game <- function(max_turns = Inf) {
     if (length(enemy_loc) != 0) {
 
       if (player_loc == enemy_loc) {
+
         hp <- hp - 1
+
+        msg <- "Struck by enemy (-1 HP)"
+
       }
 
     }
@@ -217,6 +99,8 @@ start_game <- function(max_turns = Inf) {
         if (hp > 10) {
           hp <- 10
         }
+
+        msg <- "Ate apple (+1 HP)"
 
       }
 
@@ -234,6 +118,7 @@ start_game <- function(max_turns = Inf) {
 
     .cat_room(room)
     .cat_stats(turns, hp, gold)
+    cat(msg)
 
   }
 
---
+++ R/utils.R
@@ -0,0 +1,148 @@
+
+#' Add Objects To A Room
+#' @param room Matrix. 2D room layout.
+#' @param object Character. Object to place.
+#' @noRd
+.add_object <- function(room, object = c("@", "$", "E", "a")) {
+
+  if (!inherits(room, "matrix")) {
+    stop("Argument 'room' must be a matrix.")
+  }
+
+  object <- match.arg(object)
+
+  empty_tiles <- which(room == ".")
+  sample_tile <- sample(empty_tiles, 1)
+  room[sample_tile] <- object
+
+  return(room)
+
+}
+
+#' Generate A Room From Sampled Dimensions
+#' @param what Numeric vector. Values that room height and width can take.
+#' @noRd
+.make_room <- function(room_size_limit = 6:10) {
+
+  if (!is.numeric(room_size_limit)) {
+    stop("Argument 'room_size_limit' must be a numeric vector.")
+
+  }
+
+  room_x <- sample(room_size_limit, 1)
+  room_y <- sample(room_size_limit, 1)
+  room_size <- room_x * room_y
+
+  room_1d <- rep(".", room_size)
+
+  room_2d <- matrix(room_1d, nrow = room_x, ncol = room_y)
+
+  room_2d[c(1, nrow(room_2d)), ] <- "#"
+  room_2d[, c(1, ncol(room_2d))] <- "#"
+
+  room_2d <- .add_object(room_2d, "$")
+  room_2d <- .add_object(room_2d, "E")
+  room_2d <- .add_object(room_2d, "a")
+  room_2d <- .add_object(room_2d, "@")
+
+  return(room_2d)
+
+}
+
+#' Concatenate And Print A Room Matrix
+#' @param room Matrix. 2D room layout.
+#' @noRd
+.cat_room <- function(room) {
+
+  if (!inherits(room, "matrix")) {
+    stop("Argument 'room' must be a matrix.")
+  }
+
+  for (i in 1:nrow(room)) {
+    cat(room[i, ], "\n")
+  }
+
+}
+
+#' Concatenate And Print A Stats Bar
+#' @param turns Numeric. Count of turns taken.
+#' @param hp Numeric. Count of HP remaining
+#' @param gold Numeric. Count of gold accumulated.
+#' @noRd
+.cat_stats <- function(turns, hp, gold) {
+
+  stats <- paste("HP:", hp, "| G:", gold, "\n")
+  cat(stats)
+
+}
+
+#' Record Keypress Input From User
+#' @noRd
+.accept_keypress <- function() {
+
+  keypress_support <- keypress::has_keypress_support()
+
+  legal_key <- FALSE
+
+  while(!legal_key) {
+
+    if (keypress_support) {
+      kp <- keypress::keypress()
+      legal_key <- TRUE
+    }
+
+    if (!keypress_support) {
+      kp <- readline("Direction (up, down, left, right): ")
+      legal_key <- TRUE
+    }
+
+  }
+
+  return(kp)
+
+}
+
+#' Move Player Character And Increment Counters
+#' @param room Matrix. 2D room layout.
+#' @param keypress Character.
+#' @noRd
+.move_player <- function(room, kp = c("up", "down", "left", "right")) {
+
+  if (!inherits(room, "matrix")) {
+    stop("Argument 'room' must be a matrix.")
+  }
+
+  player_loc <- which(room == "@")
+  room[player_loc] <- "."
+
+  room_y_max <- nrow(room)
+
+  if (kp %in% c("up", "down", "left", "right")) {
+
+    if (kp == "up") {
+      move_to <- player_loc - 1
+    }
+
+    if (kp == "down") {
+      move_to <- player_loc + 1
+    }
+
+    if (kp == "right") {
+      move_to <- player_loc + room_y_max
+    }
+
+    if (kp == "left") {
+      move_to <- player_loc - room_y_max
+    }
+
+    if (room[move_to] != "#") {
+      player_loc <- move_to
+    }
+
+  }
+
+  room[player_loc] <- "@"
+
+  return(room)
+
+}
